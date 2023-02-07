d9564cc1d38cb136beb3354d492620258ebc0eeb
3fuyang <fuyang@matrixorigin.cn>
2023-02-07 10:24:18 +0800

    fix: use html entities to render special chars

--- src/components/Diff.tsx
+++ src/components/Diff.tsx
@@ -8,13 +8,15 @@ interface Token {
 }
 
 /**
- * Generate JSX elements.
+ * Parse line diffs and generate diff views.
  * @param diffs Line diffs.
+ * @returns `JSX.Element`s
  */
 function generateDiffView(diffs: Change[]) {
   const diffsCpy = [...diffs]
   const result = []
 
+  /** Line Number */
   let count = 1
   while (diffsCpy.length) {
     const firstChange = diffsCpy.shift() as Change
@@ -78,6 +80,12 @@ function parseLineDiff(removed: Change, added: Change) {
   }
 }
 
+/**
+ * Parse a list of `Token`s to `JSX.Element`s.
+ * @param pres A list of `Token`s.
+ * @param side Left or right.
+ * @param lineNumber The line number of the code.
+ */
 function parsePres(pres: Token[], side: 'left' | 'right', lineNumber: number) {
   return (
     <div key={`${side}-${lineNumber}`} className={[side === 'left' ? 'bg-red-100 dark:bg-red-900' : 'bg-green-100 dark:bg-green-900', 'p-1 rounded-sm flex'].join(' ')}>
@@ -102,10 +110,17 @@ function parsePres(pres: Token[], side: 'left' | 'right', lineNumber: number) {
   )
 }
 
+/**
+ * Process whitespaces and line breaks.
+ * @param str The value of an instance of `Token`.
+ */
 function dealLineBreak(str: string) {
   let trimedStr = str
     .replace(/^(\n)*/, '')
     .replace(/\n$/, '')
+    .replace(' ', '&nbsp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
   const result: Array<JSX.Element | string> = []
   if (trimedStr.endsWith('\n')) {
     trimedStr = trimedStr.replace(/(\n)*$/, '')
@@ -118,7 +133,7 @@ function dealLineBreak(str: string) {
   }
   const segs = trimedStr.split('\n')
   result.unshift(...segs.reduce<typeof result>((prev, curr) => {
-    prev.push(<span dangerouslySetInnerHTML={{ __html: curr.replace(' ', '&nbsp;') }}></span>, <br key={curr} />)
+    prev.push(<span dangerouslySetInnerHTML={{ __html: curr }}></span>, <br key={curr} />)
     return prev
   }, []).slice(0, -1))
   return result
